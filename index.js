ff7ab29e530837f5ffdf836e0410e60dc6156b1f
kevinc2465 <chenk5104@gmail.com>
2022-11-03 13:14:06 -0400

    nothing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const DOMSelector = {
   button: document.getElementById("btn"),
+  text: document.querySelector("#parent"),
+  box: document.getElementById("project"),
 };
 
 function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
+  background.style.backgroundColor = "green";
+  text.textContent = "This is a big red box";
 }
+backgroundAndText(DOMSelector.text);
