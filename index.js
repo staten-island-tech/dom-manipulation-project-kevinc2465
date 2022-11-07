ab196bd4d1d6df2ab043c0f872734623c318e133
kevinc2465 <chenk5104@gmail.com>
2022-11-07 13:15:39 -0500

    nothing

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("container-box"),
   input: document.querySelector(`#input`),
+  reset: document.getElementById("reset"),
 };
 
-// DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an H1</h1>`);
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p> `);
