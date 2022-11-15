685c81e02d72c881852e587bad20293da31f8d19
kevinc2465 <chenk5104@gmail.com>
2022-11-15 13:14:40 -0500

    pray

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const DOMSelectors = {
   button: document.getElementById("finish"),
   box: document.getElementById("container-box"),
-  input: document.querySelector(`#input`),
+  input: document.querySelector(`#input1`),
+  data: document.querySelector(`#input2`),
+  info: document.querySelector(`#input3`),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p> `);
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p> Hello ${input}</p> `);
   DOMSelectors.input.value = "";
+  let info = DOMSelectors.info.value;
+  DOMSelectors.box.insertAdjacentHTML("afterend", `<p> ${info}</p> `);
+  DOMSelectors.info.value = "";
+  let data = DOMSelectors.data.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<p> You are ${data} years old</p> `
+  );
+  DOMSelectors.data.value = "";
 });
