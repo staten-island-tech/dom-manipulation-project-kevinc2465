cb655261943bbf12c05ce27b285765e1a7aec939
kevinc2465 <chenk5104@gmail.com>
2022-11-20 20:04:59 -0500

    pray

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const DOMSelectors = {
-  button1: document.getElementById("finish"),
-  button2: document.getElementById("reset"),
+  submit: document.getElementById("finish"),
+  reset: document.getElementById("reset"),
   box: document.getElementById("container-box"),
   input: document.querySelector(`#input1`),
+  result: document.getElementById("result"),
   data: document.querySelector(`#input2`),
   info: document.querySelector(`#input3`),
-  result: document.getElementById("result"),
 };
 
-DOMSelectors.button1.addEventListener("click", function () {
+DOMSelectors.submit.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input}</p> `);
+  DOMSelectors.result.insertAdjacentHTML("beforeend", `<p>${input}</p> `);
   DOMSelectors.input.value = "";
   let info = DOMSelectors.info.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p> ${info}</p> `);
+  DOMSelectors.result.insertAdjacentHTML("beforeend", `<p> ${info}</p> `);
   DOMSelectors.info.value = "";
   let data = DOMSelectors.data.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${data}</p> `);
+  DOMSelectors.result.insertAdjacentHTML("beforeend", `<p>${data}</p> `);
   DOMSelectors.data.value = "";
 });
 
-DOMSelectors.button2.addEventListener("click", function () {
-  document.getElementById("container-box").reset();
+DOMSelectors.reset.addEventListener("click", function (){
+    DOMSelectors.input.value = "";
+    DOMSelectors.data.value = "";
+    DOMSelectors.info.value = "";
 });
+
+
+
+
+
+
+
+
