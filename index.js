3c2160a05582f6de3942fe6f2b6c43b228a196e2
kevinc2465 <chenk5104@gmail.com>
2022-11-22 20:58:17 -0500

    nothing

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const DOMSelectors = {
-  //get form
+  form: document.getElementById("form-box"),
   submit: document.getElementById("finish"),
   reset: document.getElementById("reset"),
-  box: document.getElementById("form-box"),
-  input: document.querySelector(`#input1`),
   result: document.getElementById("result-box"),
+  input: document.querySelector(`#input1`),
   data: document.querySelector(`#input2`),
   info: document.querySelector(`#input3`),
 };
 //listen for form submit
-DOMSelectors.submit.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  console.log(DOMSelectors.input.value);
+const form = "form"; 
+console.log(DOMSelectors.form)
+
+const values = {
+  one:DOMSelectors.input.value,
+  two: DOMSelectors.data.value,
+  three: DOMSelectors.info.value,
+};
+
+
   //make 1 object from values
   //fave words object
   //pass words{} into the function that creats HTML
   //pass object into function that creates tye html
-  DOMSelectors.result.insertAdjacentHTML(
-    "beforeend",
-    `<p class="result">${DOMSelectors.input.value}</p> `
-  );
-  DOMSelectors.input.value = "";
-  let info = DOMSelectors.info.value;
-  DOMSelectors.result.insertAdjacentHTML(
-    "beforeend",
-    `<p class="result"> ${DOMSelectors.info.value}</p> `
-  );
-  DOMSelectors.info.value = "";
-  let data = DOMSelectors.data.value;
-  DOMSelectors.result.insertAdjacentHTML(
-    "beforeend",
-    `<p class="result">${DOMSelectors.data.value}</p> `
-  );
-  DOMSelectors.data.value = "";
-});
+
+  // DOMSelectors.result.insertAdjacentHTML(
+  //   "beforeend",
+  //   `<p class="result">${DOMSelectors.input.value}</p> `
+  // );
+  // DOMSelectors.input.value = "";
+  // let info = DOMSelectors.info.value;
+  // DOMSelectors.result.insertAdjacentHTML(
+  //   "beforeend",
+  //   `<p class="result"> ${DOMSelectors.info.value}</p> `
+  // );
+  // DOMSelectors.info.value = "";
+  // let data = DOMSelectors.data.value;
+  // DOMSelectors.result.insertAdjacentHTML(
+  //   "beforeend",
+  //   `<p class="result">${DOMSelectors.data.value}</p> `
+  // );
+  // DOMSelectors.data.value = "";
+
 
 DOMSelectors.reset.addEventListener("click", function () {
   DOMSelectors.input.value = "";
