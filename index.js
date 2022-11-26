f4a701665be45078383aaec1e2e9c2df3117d901
kevinc2465 <chenk5104@gmail.com>
2022-11-25 21:14:50 -0500

    revision

--- index.js
+++ index.js
@@ -3,27 +3,39 @@ const DOMSelectors = {
   submit: document.getElementById("finish"),
   reset: document.getElementById("reset"),
   result: document.getElementById("result-box"),
-  input: document.querySelector(`#input1`),
-  data: document.querySelector(`#input2`),
-  info: document.querySelector(`#input3`),
+  inputs: document.querySelectorAll(".input"),
 };
 //listen for form submit
-let values = {
-  one: DOMSelectors.input.value,
-  two: DOMSelectors.data.value,
-  three: DOMSelectors.info.value,
-};
+function makeHTML() {
+  DOMSelectors.submit.addEventListener("click", function () {
+  DOMSelectors.inputs.forEach((input) => {
+      DOMSelectors.result.insertAdjacentHTML("beforeend", `<p> ${input.value}</p>`)
+    })   
+    })};
+ makeHTML()
 
-//make 1 object from values
+
+// DOMSelectors.submit.addEventListener("click", function(){
+//   console.log(words.uno.value)
+// })
+
+//  DOMSelectors.submit.addEventListener ("click", function () {
+//   DOMSelectors.result.insertAdjacentHTML("beforeend",
+//   `<p class="result">${words.uno.value}</p> `
+// );
+  // DOMSelectors.words.value = "";
+  // console.log(values, words)
+//  });
+    //make 1 object from values
 //fave words object
 //pass words{} into the function that creats HTML
-//pass object into function that creates tye html
+//pass object into function that creates the html
 
 // DOMSelectors.result.insertAdjacentHTML(
 //   "beforeend",
 //   `<p class="result">${DOMSelectors.input.value}</p> `
 // );
-// DOMSelectors.input.value = "";
+// // DOMSelectors.input.value = "";
 // let info = DOMSelectors.info.value;
 // DOMSelectors.result.insertAdjacentHTML(
 //   "beforeend",
