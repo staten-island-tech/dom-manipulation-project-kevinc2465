3dd375558b9d170295cd42ac5ab5667a4987ee00
kevinc2465 <chenk5104@gmail.com>
2022-11-27 20:12:23 -0500

    <100

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ const DOMSelectors = {
   submit: document.getElementById("finish"),
   reset: document.getElementById("reset"),
   result: document.getElementById("result-box"),
-  // inputs: document.querySelectorAll(".point"),
-  input: document.querySelector("#input1"),
-  data: document.querySelector("#input2"),
-  info: document.querySelector("#input3"),
+  input: document.querySelector(".input1"),
+  data: document.querySelector(".input2"),
+  info: document.querySelector(".input3"),
 };
+let input = DOMSelectors.input.value;
+let data = DOMSelectors.data.value;
+let info = DOMSelectors.info.value;
 //listen for form submit
 function makeHTML() {
   DOMSelectors.submit.addEventListener("click", function () {
-    let input = DOMSelectors.input.value;
-    let data = DOMSelectors.data.value;
-    let info = DOMSelectors.info.value;
       DOMSelectors.result.insertAdjacentHTML("beforeend", `
       <div class="result">
-      <p class="individual-result"> ${input}</p> 
-      <p class="individual-result"> ${data}</p> 
-      <p class="individual-result"> ${info}</p> 
+      <p class="individual-result"> ${DOMSelectors.input.value}</p> 
+      <p class="individual-result"> ${DOMSelectors.data.value}</p> 
+      <p class="individual-result"> ${DOMSelectors.info.value}</p> 
       <button onclick="removeNameFromTheList(this)">Remove</button>
       </div>
-      `);  
-    })};
+      `);   
+      DOMSelectors.input.value="";
+      DOMSelectors.data.value="";
+      DOMSelectors.info.value="";
+    })
+
+  };
     function removeNameFromTheList(e) {
       e.parentElement.remove()
     }
  makeHTML();
-console.log(input)
 
