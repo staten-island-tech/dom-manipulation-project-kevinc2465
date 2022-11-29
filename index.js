ab15132ec0446db59b495e5d3fdc9451129aafd6
kevinc2465 <chenk5104@gmail.com>
2022-11-29 13:15:31 -0500

    asads

--- index.js
+++ index.js
@@ -6,29 +6,38 @@ const DOMSelectors = {
   input: document.querySelector(".input1"),
   data: document.querySelector(".input2"),
   info: document.querySelector(".input3"),
+  remove: document.querySelector("remove"),
 };
+
+
 let input = DOMSelectors.input.value;
 let data = DOMSelectors.data.value;
 let info = DOMSelectors.info.value;
+
 //listen for form submit
+function clear() {
+  DOMSelectors.input.value = "";
+  DOMSelectors.data.value = "";
+  DOMSelectors.info.value = "";
+}
 function makeHTML() {
   DOMSelectors.submit.addEventListener("click", function () {
-      DOMSelectors.result.insertAdjacentHTML("beforeend", `
+    DOMSelectors.result.insertAdjacentHTML(
+      "beforeend",
+      `
       <div class="result">
       <p class="individual-result"> ${DOMSelectors.input.value}</p> 
       <p class="individual-result"> ${DOMSelectors.data.value}</p> 
       <p class="individual-result"> ${DOMSelectors.info.value}</p> 
-      <button onclick="removeNameFromTheList(this)">Remove</button>
+      <button class="remove">Remove</button>
       </div>
-      `);   
-      DOMSelectors.input.value="";
-      DOMSelectors.data.value="";
-      DOMSelectors.info.value="";
-    })
+      `
+    )});
 
-  };
-    function removeNameFromTheList(e) {
-      e.parentElement.remove()
-    }
- makeHTML();
-
+function removebtn() {
+  DOMSelectors.remove.addEventListener("click", function (event) {
+    event.target.remove();
+})};
+makeHTML();
+clear();
+removebtn();
