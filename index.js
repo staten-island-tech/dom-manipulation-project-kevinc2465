b4f6d9084e4b0080dcbd168af28bcdef40507503
kevinc2465 <chenk5104@gmail.com>
2022-11-30 21:25:22 -0500

    revision to the style in js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const DOMSelectors = {
   form: document.getElementById("form-box"),
   submit: document.getElementById("finish"),
   reset: document.getElementById("reset"),
-  result: document.getElementById("result-box"),
+  resultbox: document.getElementById("result-box"),
   input: document.querySelector(".input1"),
   data: document.querySelector(".input2"),
   info: document.querySelector(".input3"),
@@ -14,32 +14,35 @@ let info = DOMSelectors.info.value;
 
 //listen for form submit
 function makeHTML() {
-  DOMSelectors.submit.addEventListener("click", function () {
-    DOMSelectors.result.insertAdjacentHTML(
+    DOMSelectors.resultbox.insertAdjacentHTML(
       "beforeend",
       `
       <div class="result">
       <p class="individual-result"> ${DOMSelectors.input.value}</p> 
       <p class="individual-result"> ${DOMSelectors.data.value}</p> 
       <p class="individual-result"> ${DOMSelectors.info.value}</p> 
-      <button class="cancel">Remove</button>
+      <button type="button" class="cancel">Remove</button>
       </div>
       `
-    );
-    DOMSelectors.input.value = "";
-    DOMSelectors.data.value = "";
-    DOMSelectors.info.value = "";
-  })
+    )
 };
-
+function clearField() {
+  DOMSelectors.input.value = "";
+  DOMSelectors.data.value = "";
+  DOMSelectors.info.value = "";
+}
 const cancels = document.querySelectorAll(".cancel");
 const result = document.querySelector(".result");
 function removebtn() {  
   cancels.forEach((cancel) => {
-    cancel.addEventListener("click", function () {
+    cancel.addEventListener("click", function (result) {
       result.remove();
+    })  
     })
-  })
 };
+DOMSelectors.submit.addEventListener("click", function () {
 makeHTML();
+clearField();
 removebtn();
+});
+    console.log(result)
